c64624a7b63570f986f34e9f697a4fe4eaa2e77e
Switch <thunyoubun@gmail.com>
2022-08-04 14:11:10 +0700

    done

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
+  const user = resp.data.results;
+  return (users = user);
 }
+
+let users;
+
+const pic = document.getElementById("img-profile");
+const names = document.getElementById("p-name");
+const email = document.getElementById("p-email");
+const add = document.getElementById("p-address");
+const btnRandom = document.getElementById("btn-random");
+
+btnRandom.onclick = async () => {
+  const resp = await axios.get("https://randomuser.me/api/");
+  const users = resp.data.results;
+  pic.src = users[0].picture.large;
+  email.innerText = users[0].email;
+  names.innerText = users[0].name.first + " " + users[0].name.last;
+  add.innerText =
+    users[0].location.city +
+    " " +
+    users[0].location.country +
+    " " +
+    users[0].location.state +
+    " " +
+    users[0].location.postcode;
+  console.log(users);
+};
